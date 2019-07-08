fc88eb1c1fc6eeecbf50bc422b55059d8dc8eb16
gautamamber <ambergautam1@gmail.com>
2019-07-09 01:02:43 +0530

    feat(login view)

---
+++ exam_app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import LoginView
+from . import views
+
+urlpatterns = [
+    path('login', LoginView.as_view(), name = "login"),
+    path('index', views.index, name = "index"),
+]
--- exam_app/views.py
+++ exam_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.http import HttpResponseRedirect
+from django.views.generic import FormView
+
+
+def index(request):
+	return render(request, "index.html")
+
+class LoginView(FormView):
+	form_class = AuthenticationForm
+	template_name = 'login.html'
+
+	def form_valid(self, form):
+		username = form.cleaned_data['username']
+		password = form.cleaned_data['password']
+		user = authenticate(username  = username, password = password)
+
+		if user is not None and user.is_active and user.is_staff:
+			login(self.request, user)
+			return HttpResponseRedirect("index")
+		else:
+			return self.form_invalid(form)
